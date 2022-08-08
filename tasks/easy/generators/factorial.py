ec5664c353b352bdd5ae88e61017770cc54209ef
unknown <sfm009@gmail.com>
2022-08-08 14:57:39 +0300

    factorial & fibonacci

--- tasks/easy/generators/factorial.py
+++ tasks/easy/generators/factorial.py
@@ -10,3 +10,14 @@ next(factorial_gen) -> 2
 next(factorial_gen) -> 6
 next(factorial_gen) -> 24
 """
+from math import factorial
+
+
+def factorial_gen(n: int):
+    for i in range(1, n + 1, 1):
+        yield factorial(i)
+
+
+for item in factorial_gen(5):
+    print(item)
+
--- tasks/easy/generators/fibonacci.py
+++ tasks/easy/generators/fibonacci.py
@@ -12,3 +12,17 @@ next(fibonacci_gen) -> 3
 next(fibonacci_gen) -> 5
 next(fibonacci_gen) -> 8
 """
+
+
+def fibonacci_gen(n: int):
+    a, b = 1, 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+
+
+for item in fibonacci_gen(8):
+    print(item)
+
+
+
