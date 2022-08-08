f15e97936aae45f8be9d3fa513ba5a7c83b84a72
unknown <sfm009@gmail.com>
2022-08-08 14:07:33 +0300

    even_numbers

--- tasks/easy/generators/even_numbers.py
+++ tasks/easy/generators/even_numbers.py
@@ -9,3 +9,14 @@ next(even_gen) -> 2
 next(even_gen) -> 4
 next(even_gen) -> 6
 """
+
+
+def get_eve_number(n):
+    for i in range(0, n, 2):
+        yield i + 2
+
+for item in get_eve_number(6):
+    print(item)
+
+
+even_gen = get_eve_number(20)
