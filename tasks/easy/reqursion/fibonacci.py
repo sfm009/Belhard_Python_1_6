0f23c99a9fa3e759bbfc50b4cf57cc7561e96d98
unknown <sfm009@gmail.com>
2022-08-08 17:02:46 +0300

    req fibonacci

--- tasks/easy/reqursion/fibonacci.py
+++ tasks/easy/reqursion/fibonacci.py
@@ -1,3 +1,12 @@
 """
 Написать рекурсивную функцию fibonacci, которая будет возвращать n-ый элемент
 """
+
+
+def fibonacci(n):
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(fibonacci(3))
