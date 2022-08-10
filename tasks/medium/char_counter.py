b2b7f82083ec0be5f24a95c77328103981d3a39c
unknown <sfm009@gmail.com>
2022-08-10 21:14:38 +0300

    chart_counter

--- tasks/medium/char_counter.py
+++ tasks/medium/char_counter.py
@@ -14,3 +14,15 @@
 Нельзя пользоваться collections.Counter!
 """
 STR_VAL = 'python is the fastest-growing major programming language'
+
+
+def count_char(STR_VAL: str):
+    count_char_dict = {}
+    str_new = "".join(set(STR_VAL))
+    for i in range(len(str_new)):
+        key = str_new[i]
+        value = STR_VAL.count(str_new[i])
+        count_char_dict[key] = value
+    return count_char_dict
+
+print(count_char(STR_VAL))
