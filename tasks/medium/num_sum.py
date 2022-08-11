e9b11c7c6aaec0a3c8931e01ac90100dff9e8501
unknown <sfm009@gmail.com>
2022-08-11 14:18:55 +0300

    num_sum

--- tasks/medium/num_sum.py
+++ tasks/medium/num_sum.py
@@ -4,3 +4,13 @@
 
 Можно пользоваться только функциями, операторами и условиями.
 """
+
+
+def sum_of_numbers(n: int):
+    if n > 0:
+        return (n % 10) + sum_of_numbers(n // 10)
+    else:
+        return 0
+
+
+print(sum_of_numbers(123))
