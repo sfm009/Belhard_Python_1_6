f918084939ff25cb3a0909bf29f318bfc9dd66c2
unknown <sfm009@gmail.com>
2022-08-12 21:44:34 +0300

    arguments

--- tasks/easy/functions/arguments.py
+++ tasks/easy/functions/arguments.py
@@ -21,15 +21,25 @@ https://pyneng.readthedocs.io/ru/latest/book/10_useful_functions/all_any.html
 
 
 def dict_from_args(*args, **kwargs):
-    if all(type(x) is int for x in args):
-        result = sum(args)
+    data = {}
+    if not all(isinstance(i,int) for i in args):
+        raise TypeError('Все позиционные аргументы должны быть целыми')
     else:
-        raise TypeError("Все позиционные аргументы должны быть целыми")
-    if all(type(x) is str for x in kwargs):
-        longest = max(kwargs, key=len)
+        data['args_sum'] = sum(args)
+    if not all(isinstance(i, str) for i in kwargs.values()):
+        raise TypeError('Все аргументы - ключевые слова должны быть строками')
     else:
-        raise TypeError("Все аргументы - ключевые слова должны быть строками")
-    return {"args_sum": result, "kwargs_max_len": longest}
+        data['kwargs_max_len'] = len(max(kwargs.values(), key=len))
+    return data
+    # if all(type(x) is int for x in args):
+    #     result = sum(args)
+    # else:
+    #     raise TypeError("Все позиционные аргументы должны быть целыми")
+    # if all(type(x) is str for x in kwargs):
+    #     longest = max(kwargs, key=len)
+    # else:
+    #     raise TypeError("Все аргументы - ключевые слова должны быть строками")
+    # return {"args_sum": result, "kwargs_max_len": longest}
 
 
-print(dict_from_args(1, 2, 3, first='aaa', second='bbb', third=3))
+print(dict_from_args(1, 2, 3, first='aaa', second='bbb', third='ccc'))
